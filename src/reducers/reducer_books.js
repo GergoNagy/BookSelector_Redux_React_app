23e4f0b15869129f843a6da8f70fa92d5d9ae2a4
Gergo Nagy <guzmop@gmail.com>
2017-11-01 20:14:37 +0000

    add pages for the book details

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,10 +1,10 @@
 export default function (){
     return [
-        {title:'Book one'},
-        {title:'Book cica'},
-        {title:'Dragon'},
-        {title:'Hobbit'},
-        {title:'Ize book'}
+        {title:'Book one', page: 100},
+        {title:'Book cica', page: 90},
+        {title:'Dragon', page: 44},
+        {title:'Hobbit', page: 33},
+        {title:'Ize book', page: 1}
     ]
 }
 
