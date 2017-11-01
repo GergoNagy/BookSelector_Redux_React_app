08432afad37a9634dcb521e66103478b50600b36
Gergo Nagy <guzmop@gmail.com>
2017-11-01 19:08:53 +0000

    Create the first reducer end connect with the book list

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function (){
+    return [
+        {title:'Book one'},
+        {title:'Book cica'},
+        {title:'Dragon'},
+        {title:'Hobbit'},
+        {title:'Ize book'}
+    ]
+}
+
